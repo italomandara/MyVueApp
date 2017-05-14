576415ab8a24626c083abbb98dcdae50dd52212e
Italo Mandara <italomandara@gmail.com>
2017-05-14 19:46:06 +0100

    build inside django static folder

--- config/index.js
+++ config/index.js
@@ -4,10 +4,12 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve('../mysite/static/vue/index.html'),
+    assetsRoot: path.resolve('../mysite/static/vue'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '../mysite/static/vue/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
